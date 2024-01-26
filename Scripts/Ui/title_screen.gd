d9fcbe0245fca530ea9abc2199cd2a8aa9574a1b
Emmanuel Diehl <emmanuel.diehl@ifsr.de>
2024-01-26 18:30:05 +0100

    add title screen

--- Scripts/Ui/title_screen.gd
+++ Scripts/Ui/title_screen.gd
@@ -1,11 +1,4 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_repo_link_button_pressed():
+	OS.shell_open("https://github.com/emmdie/GlobalGamejam23")
