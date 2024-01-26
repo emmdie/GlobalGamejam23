42aacf684264e5e38fdcd3d80a5f221293a2f61e
Emmanuel Diehl <emmanuel.diehl@ifsr.de>
2024-01-26 20:53:00 +0100

    add script to enemy

--- Scripts/Enemies/enemy.gd
+++ Scripts/Enemies/enemy.gd
@@ -1,2 +1,13 @@
 extends Sprite3D
 
+@export var movement_speed = 100
+
+var direction
+var target
+
+func _process(delta):
+	if target != null:
+		move_towards_player(delta)
+		
+func move_towards_player(delta):
+	pass
