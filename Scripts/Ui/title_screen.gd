266bb1ed05ca9b5bc9b65a6d87ce04cf76277d72
Emmanuel Diehl <emmanuel.diehl@ifsr.de>
2024-01-26 18:46:23 +0100

    connect credits button to github doc

--- Scripts/Ui/title_screen.gd
+++ Scripts/Ui/title_screen.gd
@@ -4,4 +4,7 @@ func _on_repo_link_button_pressed():
 	OS.shell_open("https://github.com/emmdie/GlobalGamejam23")
 
 func _on_credits_button_pressed():
-	OS.shell_open("google.de")
+	OS.shell_open("https://github.com/emmdie/GlobalGamejam23/blob/main/credits.md")
+
+func _on_play_button_pressed():
+	pass # Replace with function body.
