2b569261400b694eedebcb1217d025242a697458
Emmanuel Diehl <emmanuel.diehl@ifsr.de>
2024-01-28 00:10:05 +0100

    add final slide

--- Scripts/Ui/story_slide.gd
+++ Scripts/Ui/story_slide.gd
@@ -1,11 +1,13 @@
 extends Control
 
+enum STATE {PLAYING, FINISHED}
+
 @onready var text = $ParallaxBackground/ParallaxLayer/SlideTextDisplay
 @onready var parallax = $ParallaxBackground
 @onready var keyboardSound = $keyBoardSound
-enum STATE {PLAYING, FINISHED}
+@onready var state = STATE.PLAYING
 
-var state
+signal next_slide
 
 func _ready():
 	text.visible_characters = 0
@@ -15,6 +17,16 @@ func _process(delta):
 	
 func _on_character_timer_timeout():
 	if text.get_total_character_count() == text.visible_characters:
+		state = STATE.FINISHED
+		$CharacterTimer.queue_free()
 		return
 	text.visible_characters += 1
 	keyboardSound.play()
+
+func _on_next_button_pressed():
+	if state == STATE.PLAYING:
+		state = STATE.FINISHED
+		text.visible_characters = text.get_total_character_count()
+	else:
+		next_slide.emit()
+		
