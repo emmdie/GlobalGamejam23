92380e59674ac2e94c30c25577bbdcac0fd98dce
Emmanuel Diehl <emmanuel.diehl@ifsr.de>
2024-01-27 22:52:19 +0100

    add story slides

--- Scripts/Ui/story_slide.gd
+++ Scripts/Ui/story_slide.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var text = $ParallaxBackground/ParallaxLayer/SlideTextDisplay
 @onready var parallax = $ParallaxBackground
+@onready var keyboardSound = $keyBoardSound
 enum STATE {PLAYING, FINISHED}
 
 var state
@@ -13,7 +14,7 @@ func _process(delta):
 	parallax.scroll_offset = get_global_mouse_position()*0.14
 	
 func _on_character_timer_timeout():
-	if text.text.get_total_character_count() == text.visible_characters:
+	if text.get_total_character_count() == text.visible_characters:
 		return
 	text.visible_characters += 1
-	
+	keyboardSound.play()
