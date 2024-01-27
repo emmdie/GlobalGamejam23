88ff922c0821591a4bac82d18a101cd3266e4e42
Emmanuel Diehl <emmanuel.diehl@ifsr.de>
2024-01-27 18:22:22 +0100

    implement pause Menu

--- Scripts/Ui/LevelWonScreen.gd
+++ Scripts/Ui/LevelWonScreen.gd
@@ -4,6 +4,7 @@ signal main_menu
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
 func _on_main_menu_button_pressed():
 	get_tree().paused = false
 	main_menu.emit()
--- Scripts/Ui/PauseScreen.gd
+++ Scripts/Ui/PauseScreen.gd
@@ -1,5 +1,19 @@
 extends Control
 
-func _on_texture_button_pressed():
+signal main_menu
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+func _on_continue_button_pressed():
 	get_tree().paused = false
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	queue_free()
+	
+func _on_quit_game_button_2_pressed():
+		get_tree().quit()
+
+func _on_main_menu_button_pressed():
+	get_tree().paused = false
+	main_menu.emit()
 	queue_free()
